f49e33b2aef5f7cc2c9f1734e149c48494b86be6
A. <Alekseew.zheka0207@gmail.com>
2025-06-10 16:31:33 +0300

    Add project files.

---
+++ Parallel Monitor of Files in Directory/MonitorDirectory.cs
@@ -0,0 +1,25 @@
+﻿namespace Parallel_Monitor_of_Files_in_Directory;
+
+internal class MonitorDirectory
+{
+    public string DirectoryPath { get; private set; }
+    public IEnumerable<string> FilesNames { get; private set; }
+    private Thread _monitorThread;
+    private CancellationToken? ct;
+    public MonitorDirectory(string directoryPath, CancellationToken? ct)
+    {
+        DirectoryPath = directoryPath;
+        FilesNames = Directory.GetFiles(DirectoryPath);
+        _monitorThread = new Thread(MonitorFiles);
+        _monitorThread.Start(ct);
+    }
+
+
+    private void MonitorFiles()
+    {
+        while (true || )
+        {
+
+        }
+    }
+}
